b124de5fc002b10b3d3d11f47147d8998edbfb4c
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-14 21:22:01 +0600

    Create multi_test.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ samples/multi_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ==============
+#      Example of multithreaded/multiprocessed data fetching.
+#      For ratelimiter testing mostly :)
+# ==============
+
+import concurrent.futures
+import cvedb
+
+cvedb_api = cvedb.Cvedb(api_key="YOUR_API_KEY_HERE")
+
+collection_names = cvedb_api.collections()[:20]
+
+query_pool = ["type:%s" % collection for collection in collection_names]
+merged_results = []
+
+# Using parallel Thread pool for testing thread safety
+with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    search_results_pool = [executor.submit(cvedb_api.search, query, 10) for query in query_pool]
+    for future in concurrent.futures.as_completed(search_results_pool):
+        print("Collected %s results with Thread Pool" % len(future.result()))
+        merged_results.append(future.result())
+
+# Using parallel Process pool for testing thread safety
+with concurrent.futures.ProcessPoolExecutor(max_workers=20) as executor:
+    search_results_pool = [executor.submit(cvedb_api.search, query, 10) for query in query_pool]
+    for future in concurrent.futures.as_completed(search_results_pool):
+        print("Collected %s results with Multiprocess Pool" % len(future.result()))
+        merged_results.append(future.result())
