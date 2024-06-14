2c987f130a7822c84bd9a925a6305b490df1d562
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-14 21:45:29 +0600

    Update search.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- samples/search.py
+++ samples/search.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# ==============
-#      Cvedb search API usage example
-# ==============
+
+# Cvedb search API usage example
+
+import os
 
 import cvedb
 
-cvedb_api = cvedb.Cvedb(api_key="YOUR_API_KEY_HERE")
-possible_autocomplete = cvedb_api.autocomplete("heartbleed")
-heartbleed_related = cvedb_api.search("heartbleed", limit=10)
-total_heartbleed = heartbleed_related.total  # Notice you can do this because of Cvedb' own AttributeList type
-CVE_2017_14174 = cvedb_api.document("CVE-2017-14174")
+cvedb_api = cvedb.CvedbApi(api_key=os.environ["KEY"])
+possible_autocomplete = cvedb_api.query_autocomplete("heartbleed")
+heartbleed_related = cvedb_api.find("heartbleed", limit=10)
+total_heartbleed = heartbleed_related.total
+CVE_2017_14174 = cvedb_api.get_bulletin("CVE-2017-14174")
